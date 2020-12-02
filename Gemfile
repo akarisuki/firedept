fcb1da5ed43d0d81ad5b1839b5344914610439f1
akarisuki <zakiimo0712@yahoo.co.jp>
2020-12-02 11:26:46 +0900

    Rspec導入

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.1'
+ruby '2.7.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.6.0'
+  gem 'spring-commands-rspec'
 end
 
 group :development do
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const  vue = require('./loader/vue')
+
+environment.plugins.prepend('VueLoaderPlugin',new VueLoaderPlugin())
+environment.loader.prepend('vue',vue)
+
+//　Vue.js フル版(Compiler入り)
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// jqueryとBootstrapのJSを使えるように
+const webpack = require('webpeck')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
 
 module.exports = environment
